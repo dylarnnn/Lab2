994f1764e0575e1ebc5540df16e980d1b93fda3c
DylanChoo <DYLANCHOOTL.25@ichat.sp.edu.sg>
2026-04-27 14:25:42 +0800

    bmi.py

---
+++ bmi.py
@@ -0,0 +1,13 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight)) # + can only support string variable
+    BMI = weight / (height ** 2) #use float() to convert str to float
+    print("BMI = ", BMI)
+
+    if BMI < 18.5:
+        print("You are underweight")
+    elif BMI >= 18.5 and BMI <= 25.0:
+        print("You are at normal weight")
+    elif BMI > 25.0:
+        print("You are overweight")
+calculate_bmi(weight = 57, height = 1.73)
