3f32adfbc095a5ac26001d48faeb8f3eabdd06f0
DylanChoo <DYLANCHOOTL.25@ichat.sp.edu.sg>
2026-05-04 14:26:19 +0800

    week3updated

--- bmi.py
+++ bmi.py
@@ -6,10 +6,13 @@ def calculate_bmi(height, weight):
 
     if BMI < 18.5:
         print("You are underweight")
+        return -1
     elif BMI >= 18.5 and BMI <= 25.0:
         print("You are at normal weight")
+        return 0
     elif BMI > 25.0:
         print("You are overweight")
+        return 1
 def main():
     calculate_bmi(weight = 57, height = 1.73)
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
